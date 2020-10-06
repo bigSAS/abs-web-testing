90a7e685f0874c69d0f770fab202dbb2b2a6a603
bigSAS <sas.it.tomasz.majk@gmail.com>
2020-10-06 08:10:22 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
   name = 'abs-web-testing',         # How you named your package folder (MyLib)
-  packages = ['abs-web-testing'],   # Chose the same as "name"
+  packages = ['abswt'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Action Based Selenium WebTesting library - selenium in more accessible way :)',   # Give a short description about your library
